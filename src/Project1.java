78b9bceb5bf28542917552f2bfe248003da63bfb
auzman466 <auzman446@gmail.com>
2020-02-19 20:38:24 -0500

    finsihed it ayy

--- src/Project1.java
+++ src/Project1.java
@@ -16,9 +16,10 @@ public class Project1 {
         return file;
     }
 
-//    public static String readLex(int line, int index) {
-//
-//    }
+    public static void print(int line, int index, String type, StringBuilder token) {
+        index = index + 1;
+        System.out.println("Line" + line + ": " + index + " " + " " + type + ": " + token.toString());
+    }
 
     public static void main(String[] args) throws FileNotFoundException {
         Scanner scan = new Scanner(System.in);
@@ -34,12 +35,14 @@ public class Project1 {
                 // check if character is a digit
                 boolean checkDigit = Character.isDigit(line.charAt(i));
                 boolean checkAlpha = Character.isAlphabetic(line.charAt(i));
-                if (checkDigit) {
+                if (line.charAt(i) == ' ') {
+                    continue;
+                } else if (checkDigit) {
                     // begin parsing digits in sequence
-                    // add first digit of number to a string
-                    analyzedToken = new StringBuilder();
+                    analyzedToken.delete(0, analyzedToken.length());
                     for (int j = i; j < line.length(); j++) {
                         // check for decimal; if decimal present, type is double.
+                        // add first digit of number to a string
                         boolean checkDigit1 = Character.isDigit(line.charAt(j));
                         if (line.charAt(j) == '.') {
                             analyzedToken.append(line.charAt(j));
@@ -52,8 +55,9 @@ public class Project1 {
                             analyzedToken.append(line.charAt(j));
                             analyzedType = "int constant";
                         } else {// constant has ended
-                            System.out.println("Line" + lineCount + ": " + i + " " + " " + analyzedType + ": " + analyzedToken.toString());
-                            i = j; // move index i to index j to continue parsing line without retreading ground
+                            print(lineCount, i, analyzedType, analyzedToken);
+                            //System.out.println("Line" + lineCount + ": " + i+1 + " " + " " + analyzedType + ": " + analyzedToken.toString());
+                            i = j - 1; // move index i to index j to continue parsing line without retreading ground
                             break;
                         }
 
@@ -61,24 +65,74 @@ public class Project1 {
                     }
 
                 } else if (checkAlpha) {
-                    analyzedToken = new StringBuilder();
+                    boolean keyword = false;
+                    //check first if the following characters spell a keyword
+                    analyzedToken.delete(0, analyzedToken.length());
                     if (i + 4 < line.length()) {
                         if (line.substring(i, i + 4).equals("int ")) {
-                            System.out.println(line.substring(i, i + 4));
+                            analyzedType = "keyword";
+                            analyzedToken.append("int");
+                            print(lineCount, i, analyzedType, analyzedToken);
+                            i = i + 3;
+                            keyword = true;
+                            //System.out.println("Line" + lineCount + ": " + i+1 + " " + " " + analyzedType + ": " + analyzedToken.toString());
                         }
-                    } else if (i + 7 < line.length()) {
-                        if (line.substring(i, i + 7).equals("double")) {
-                            System.out.println(line.substring(i, i + 7));
-                        }
-                        else if (line.substring(i, i + 7).equals("String")) {
-                            System.out.println(line.substring(i, i + 7));
-                        }
-                    } else {
-//                        for (int j = i; j < line.length(); j++) {
-//                        }
                     }
+                    if (i + 7 < line.length()) {
+                        if (line.substring(i, i + 7).equals("double ")) {
+                            analyzedType = "keyword";
+                            analyzedToken.append("double");
+                            print(lineCount, i, analyzedType, analyzedToken);
+                            i = i + 6;
+                            keyword = true;
+                        } else if (line.substring(i, i + 7).equals("String ")) {
+                            analyzedType = "keyword";
+                            analyzedToken.append("string");
+                            print(lineCount, i, analyzedType, analyzedToken);
+                            i = i + 6;
+                            keyword = true;
+                        }
+                    }
+                    if (!keyword) {
+                        for (int j = i; j < line.length(); j++) {
+                            boolean checkAlpha1 = Character.isAlphabetic(line.charAt(j));
+                            if (checkAlpha1) {
+                                analyzedToken.append(line.charAt(j));
+                            } else {
+                                analyzedType = "identifier";
+                                print(lineCount, i, analyzedType, analyzedToken);
+                                i = j-1;
+                                break;
+                            }
+                        }
+                    }
+                } else if (line.charAt(i) == '=' || line.charAt(i) == '(' || line.charAt(i) == ')' || line.charAt(i) == '+' || line.charAt(i) == '-' || line.charAt(i) == '*' || line.charAt(i) == '/' || line.charAt(i) == ',' || line.charAt(i) == ';') {
+                    analyzedType = "operator";
+                    analyzedToken.delete(0, analyzedToken.length());
+                    analyzedToken.append(line.charAt(i));
+                    print(lineCount, i, analyzedType, analyzedToken);
+                } else if (line.charAt(i) == '\"'){
+                    analyzedToken.delete(0, analyzedToken.length());
+                    analyzedType = "string constant";
+                    analyzedToken.append(line.charAt(i));
+                    for (int j = i+1; j < line.length(); j++){
+                        if (line.charAt(j) != '\"'){
+                            analyzedToken.append(line.charAt(j));
+                        } else {
+                            analyzedToken.append(line.charAt(j));
+                            print(lineCount, i, analyzedType, analyzedToken);
+                            i = j;
+                            break;
+                        }
+                    }
+                } else {
+                    analyzedToken.delete(0, analyzedToken.length());
+                    analyzedToken.append(line.charAt(i)).append(" not recognized");
+                    analyzedType = "error";
+                    print(lineCount, i, analyzedType, analyzedToken);
                 }
             }
+
         }
     }
 }
